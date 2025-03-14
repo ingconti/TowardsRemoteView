3dd6d033c521bbcf8ff1019966bdab89745e183e
ing.conti <ingconti@ingconti.com>
2025-03-14 12:48:11 +0100

    added Automaton

---
+++ src/main/java/it/polimi/towardsremoteview/Automaton.java
@@ -0,0 +1,59 @@
+package it.polimi.towardsremoteview;
+
+
+public class Automaton {
+
+    private Boolean paid = false;
+    private DinnerPhase state = DinnerPhase.ENTREE;
+
+    public DinnerPhase getState(){
+        return state;
+    }
+
+    private Boolean canEvolve(){
+        int currOrd = state.ordinal();
+        int dessertOrd = DinnerPhase.DESSERT.ordinal();
+
+        if (currOrd >= dessertOrd && !paid) {
+            System.out.println("PAY BEFORE!!!");
+            return false;
+        }
+        return true;
+    }
+
+    public void setPaid(){
+        paid = true;
+    }
+
+
+    public Boolean evolve() {
+        if (!canEvolve())
+            return false;
+
+        int currOrd = state.ordinal();
+        int lastOrd = DinnerPhase.THE_END_OF_LUNCH.ordinal();
+
+        if (currOrd < lastOrd) {
+            state = state.next();
+            return true;
+        }
+        return false;
+    }
+
+
+    public Boolean evolveTo(DinnerPhase toState){
+        if (!canEvolve())
+            return false;
+
+        int toOrd = toState.ordinal();
+        int currOrd = state.ordinal();
+
+        if (toOrd>currOrd) {
+            state = toState;
+            return true;
+        }
+        return false;
+    }
+
+
+}
