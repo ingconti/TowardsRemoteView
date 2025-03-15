73d4a8a5d618d68e915d1be44cc0eed37f09fd0e
ing.conti <ingconti@ingconti.com>
2025-03-15 12:10:18 +0100

    Added Server And Refactoring..

---
+++ src/main/java/it/polimi/towardsremoteview/ServerMain.java
@@ -0,0 +1,60 @@
+package it.polimi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    static int portNumber = 1234;
+
+    public static void main(String[] args) {
+
+        System.out.println("Server Started!");
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Listening on port " + portNumber);
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Accepted");
+
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // waits for data and reads it in until connection dies
+        // readLine() blocks until the server receives a new line from client
+        String s = "";
+        try {
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+                out.println(s.toUpperCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // we should close..
+    }
+}
