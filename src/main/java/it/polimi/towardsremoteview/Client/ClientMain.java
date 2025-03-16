fc6ad896fd637bb6fc5b75c49566b7d512d41f67
ing.conti <ingconti@ingconti.com>
2025-03-16 21:02:49 +0100

    refactored

---
+++ src/main/java/it/polimi/towardsremoteview/Client/ClientMain.java
@@ -0,0 +1,42 @@
+package it.polimi.towardsremoteview;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new
+                FXMLLoader(ClientMain.class.getResource("hello-view.fxml"));
+
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        //was: controller.setStage(stage);
+        controller.setStage(stage, this.virtualServer);
+
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+        // added:
+        this.virtualServer.start();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+    VirtualServer   virtualServer;
+    public ClientMain() {
+        this.virtualServer = new VirtualServer();
+    }
+}
---
+++ src/main/java/it/polimi/towardsremoteview/Client/ClientThread.java
@@ -0,0 +1,26 @@
+package it.polimi.towardsremoteview;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ClientThread extends Thread {
+    private BufferedReader reader;
+
+    public ClientThread(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public void run() {
+        System.out.println("ClientThread started");
+        int i = 0;
+
+        while (true) {
+            try {
+                String answer = this.reader.readLine();
+                System.out.println(answer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
